d5b71a9132eed1b39a7b5242e9f17f4a21f7362f
ehsan <ehsanesfhany@gmail.com>
2025-01-06 17:45:00 +0330

    Writing flip-flop module D and writing tests for it

--- Vending_machine.v
+++ Vending_machine.v
@@ -1,4 +1,47 @@
-module Vending_machine (
-);
-    
+module DFF (Q, D, clk, rst);
+    output Q;
+    input D, clk, rst;
+    wire Qm, S, R;
+
+    nand (S, D, clk, rst);
+    nand (R, ~D, clk, rst);
+    nand (Qm, R, Q);
+    nand (Q, S, Qm);
+
+endmodule
+
+
+module testbench;
+    reg D, clk, rst;
+    wire Q;
+
+    DFF dff (Q, D, clk, rst);
+
+    initial begin
+
+        D = 0; clk = 0; rst = 0;
+
+     
+        #5 rst = 1;
+
+        #5 D = 0;
+        #5 D = 1; 
+        #5 D = 0; 
+        #5 D = 1; 
+        #5 D = 0; 
+        #5 D = 1; 
+        #5 D = 0;
+        #5 D = 1;
+        #5 D = 0; 
+
+
+
+        #20 $finish;
+    end
+
+    always #5 clk = ~clk; 
+
+    initial begin
+        $monitor("At time %t, Q = %d, D = %d, clk = %d, rst = %d", $time, Q, D, clk, rst);
+    end
 endmodule
